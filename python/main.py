30c71609d1d9ad63e32edbfd1fdadc9f22a0ab9f
Pilter Barnabás <pilter.barnabas.jozsef@students.jedlik.eu>
2024-03-08 11:26:43 +0100

    Alapozás, munka kiosztása

--- python/main.py
+++ python/main.py
@@ -1,1 +1,9 @@
 from osztaly import Tarskereso
+
+emberek: list[Tarskereso] = []
+
+f = open('emberek.csv', 'r', encoding='utf-8')
+for sor in f:
+    emberek.append(Tarskereso(sor))
+f.close()
+
---
+++ readme.md
@@ -0,0 +1,6 @@
+# HTML
+- Zsombi: Kezdőlap
+- Gábor: Működése
+- Barni: Rólunk
+
+# Python
