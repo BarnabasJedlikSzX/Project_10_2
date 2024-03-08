acbd9f4fe9d02ba752f1b9009a1860a44fb7200b
Pilter Barnabás <pilter.barnabas.jozsef@students.jedlik.eu>
2024-03-08 11:20:29 +0100

    Osztály haladás

--- python/osztaly.py
+++ python/osztaly.py
@@ -3,5 +3,8 @@ class Tarskereso:
         adatok = sor.strip().split(';')
         self.nev = adatok[0]
         self.kor = adatok[1]
-        self.lakohely = adatok[2]
+        self.tavolsag_toled = int(adatok[2])
         self.eletkor = adatok[3]
+        self.nem = adatok[4]
+        self.keresett_nem = adatok[5]
+        self.keresett_kor = adatok[5]
