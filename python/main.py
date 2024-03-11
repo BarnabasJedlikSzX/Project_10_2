bf608d94157692b01e43af95b26140f9d5097d24
Pintér Gábor <pinter.gabor@students.jedlik.eu>
2024-03-11 14:05:30 +0100

    #1 main kezdes, megnezni a szurest

--- python/main.py
+++ python/main.py
@@ -1,9 +1,39 @@
 from osztaly import Tarskereso
+from login import login
+import os
 
 emberek: list[Tarskereso] = []
 
-f = open('emberek.csv', 'r', encoding='utf-8')
-for sor in f:
-    emberek.append(Tarskereso(sor))
-f.close()
+def main():
+    beolvasas()
+    nev = login()
+    kilep = False
+    index = 0
+    bejelentkezve: Tarskereso = None
+    for e in emberek:
+        if e.felhasznalonev == nev:
+            bejelentkezve = e
+            break 
+    while not kilep:
+        kovetkezo = emberek[index]
+        if lehet(kovetkezo, bejelentkezve):     # felajánlhatja az embert
+            os.system('cls')
+            print(f'{kovetkezo.vezeteknev} {kovetkezo.keresztnev}')
+            print(f'Kor: {kovetkezo.kor}')
+            
 
+    
+
+
+def beolvasas():
+    f = open('szingli_anyukak_es_apukak_es_gyerekek.csv', 'r', encoding='utf-8')
+    for sor in f:
+        emberek.append(Tarskereso(sor))
+    f.close()
+
+def lehet(szemely: Tarskereso, jelenlegi):
+    if szemely == jelenlegi or jelenlegi.keresett_nem != szemely.nem or jelenlegi.keresett_kor_also_hatar < szemely.kor or jelenlegi.keresett_kor_felso_hatar > szemely.kor:
+        return False
+    
+
+main()
