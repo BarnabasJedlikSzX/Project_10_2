efc4bbf7ce706761dfb5001cc722df6f37c4480a
Pilter Barnabás <pilter.barnabas.jozsef@students.jedlik.eu>
2024-03-11 13:46:50 +0100

    csv és osztály

--- python/osztaly.py
+++ python/osztaly.py
@@ -1,10 +1,13 @@
 class Tarskereso:
     def __init__(self, sor) -> None:
         adatok = sor.strip().split(';')
-        self.nev = adatok[0]
-        self.kor = adatok[1]
-        self.tavolsag_toled = int(adatok[2])
-        self.eletkor = adatok[3]
-        self.nem = adatok[4]
-        self.keresett_nem = adatok[5]
-        self.keresett_kor = adatok[5]
+        self.vezeteknev = adatok[0]
+        self.keresztnev = adatok[1]
+        self.kor = adatok[2]
+        self.tavolsag_toled = int(adatok[3])
+        self.nem = bool(adatok[4])
+        self.keresett_nem = bool(adatok[5])
+        self.keresett_kor_also_hatar = int(adatok[6])
+        self.keresett_kor_felso_hatar = int(adatok[7])
+        self.felhasznalonev = adatok[8]
+        self.jelszo = adatok[9]
