a7409578700e08c7e8669d4da4166682b2304794
Pintér Gábor <pinter.gabor@students.jedlik.eu>
2024-03-12 12:11:53 +0100

    #6 alap felhasználók randomolása

--- python/main.py
+++ python/main.py
@@ -1,5 +1,6 @@
 from osztaly import Tarskereso
 from login import login
+from random import randint
 import os
 
 emberek: list[Tarskereso] = []
@@ -20,20 +21,44 @@ def main():
             os.system('cls')
             print(f'{kovetkezo.vezeteknev} {kovetkezo.keresztnev}')
             print(f'Kor: {kovetkezo.kor}')
+            print(f'Távolság tőled: {kovetkezo.tavolsag_toled} km')
             
 
     
 
 
 def beolvasas():
-    f = open('szingli_anyukak_es_apukak_es_gyerekek.csv', 'r', encoding='utf-8')
+    f = open('python/szingli_anyukak_es_apukak_es_gyerekek.csv', 'r', encoding='utf-8')
     for sor in f:
         emberek.append(Tarskereso(sor))
     f.close()
 
-def lehet(szemely: Tarskereso, jelenlegi):
+def lehet(szemely: Tarskereso, jelenlegi: Tarskereso):
     if szemely == jelenlegi or jelenlegi.keresett_nem != szemely.nem or jelenlegi.keresett_kor_also_hatar < szemely.kor or jelenlegi.keresett_kor_felso_hatar > szemely.kor:
         return False
+    return True
+
+def randomolas():
+    f = open('python/szingli_anyukak_es_apukak_es_gyerekek.csv', 'r', encoding='utf-8')
+    vezeteknevek: list[str] = []
+    keresztnevek: list[str] = []
+    korok: list[str] = []
+    nemek: list[str] = []
+    keresett_nemek: list[str] = []
+    tavolsagok: list[str] = []
+    for sor in f:
+        adatok = sor.strip().split(';')
+        vezeteknevek.append(adatok[0])
+        keresztnevek.append(adatok[1])
+        korok.append(adatok[2])
+        tavolsagok.append(adatok[3])
+        nemek.append(adatok[4])
+        keresett_nemek.append(adatok[5])
+    f.close()
+    f = open('python/szingli_anyukak_es_apukak_es_gyerekek.csv', 'w', encoding='utf-8')
+    for i in range(0, 50):
+
+        f.write(f'{vezeteknevek[i]};{keresztnevek[i]};{korok[i]};{tavolsagok[i]};{nemek[i]};{keresett_nemek[i]};{max(18, int(korok[i])-7)};{min(70, int(korok[i])+7)};{keresztnevek[i]};{keresztnevek[i].lower()}123\n')
     
 
-main()
+randomolas()
--- python/osztaly.py
+++ python/osztaly.py
@@ -5,7 +5,7 @@ class Tarskereso:
         self.keresztnev = adatok[1]
         self.kor = adatok[2]
         self.tavolsag_toled = int(adatok[3])
-        self.nem = bool(adatok[4])
+        self.nem = bool(adatok[4])      #0: nő   1: férfi
         self.keresett_nem = bool(adatok[5])
         self.keresett_kor_also_hatar = int(adatok[6])
         self.keresett_kor_felso_hatar = int(adatok[7])
