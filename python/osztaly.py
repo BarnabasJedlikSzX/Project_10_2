127e0646516ffb0486f59504aa0f54d768d9a54d
Pintér Gábor <pinter.gabor@students.jedlik.eu>
2024-03-22 11:35:44 +0100

    #7 szűrés, gyerekszám

--- python/osztaly.py
+++ python/osztaly.py
@@ -9,3 +9,6 @@ class Tarskereso:
         self.keresett_nem = bool(adatok[5])
         self.keresett_kor_also_hatar = int(adatok[6])
         self.keresett_kor_felso_hatar = int(adatok[7])
+        self.gyerekek = int(adatok[8])
+        self.keresett_gyerek_felso_hatar = int(adatok[9])
+        self.keresett_gyerek_also_hatar = int(adatok[10])
