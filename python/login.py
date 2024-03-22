ec8d15205440289095eb73e458146f2cb4762826
Tóth Zsombor Balázs <toth.zsombor.balazs@students.jedlik.eu>
2024-03-22 11:33:19 +0100

    #2 Loginpanel kész

--- python/login.py
+++ python/login.py
@@ -1,3 +1,4 @@
+from osztaly import Tarskereso
 from loginAdatok import Data
 import os
 from random import randint
@@ -97,15 +98,14 @@ def regisztracio():
     input('<ENTER>')
     
 
-def hasheles():
-    f = open('python/login.csv', 'w', encoding='utf-8')
+'''
+def login_feltoltes():
+    f = open('python/szingli_anyukak_es_apukak_es_gyerekek.csv', 'r', encoding='utf-8')
+    f2 = open('python/login.csv', 'w', encoding='utf-8')
+    hashelt = None
     for sor in f:
-        hashelt = Data(sor).jelszo
-        rejtett = hash(hashelt)
-        f.write(f'{Data(sor).felhasznalonev};{rejtett}\n')
-    f.close()
-    print('Kész')
-hasheles()
-
+        hashelt = hash(f'{Tarskereso(sor).keresztnev.lower()}123')
+        f2.write(f'{Tarskereso(sor).keresztnev};{hashelt}\n')
+'''
 
 main()
