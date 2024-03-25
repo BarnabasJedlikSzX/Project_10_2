e06754450a402c4ee11f0ed9ba9e4cf51e0e4368
Pintér Gábor <pinter.gabor@students.jedlik.eu>
2024-03-25 13:59:00 +0100

    #8 #7 eddigi szűrők változtatása, main folyamatossága

--- python/main.py
+++ python/main.py
@@ -1,5 +1,5 @@
 from osztaly import Tarskereso
-#from login import *
+# from login import *
 from random import randint
 import os
 
@@ -15,30 +15,31 @@ def main():
         login()
     else:
         regisztracio()'''
-    nev = input()
+    vezeteknev = input()
+    keresztnev = input()
     kilep = False
-    bejelentkezve: Tarskereso = None
-    for e in emberek:
-        if e.keresztnev == nev:      # visszaállítani login.felhasznalonev-re és e.felhasznalonev-re
-            bejelentkezve = e
-            break 
-    index = 0
-    valasztas = ''
-    
-    print('Opciók: ')
-    print('\t1.: Emberek böngészése')
-    print('\t2.: Szűrők változtatása')
-    print('\t3.: Kilépés')
-    opcio = input('Választásom: ')
-    while opcio != '1' and opcio != '2' and opcio != '3':
-        opcio = input('Választásom')
-    match opcio:
-        case '1':
-            emberek_ajanlasa(bejelentkezve)
-        case '2':
-            szures(bejelentkezve)
-        case '3':
-            pass
+    bejelentkezve_index = 0
+    for i in range(0, len(emberek)):
+        if emberek[i].keresztnev == keresztnev and emberek[i].vezeteknev == vezeteknev:
+            bejelentkezve_index = i
+            break
+    opcio = ''
+    while opcio != '0':
+        os.system('cls')
+        print('Opciók: ')
+        print('\t1.: Emberek böngészése')
+        print('\t2.: Szűrők változtatása')
+        print('\t0.: Kilépés')
+        opcio = input('Választásom: ')
+        while opcio != '1' and opcio != '2' and opcio != '0':
+            opcio = input('Választásom: ')
+        match opcio:
+            case '1':
+                emberek_ajanlasa(emberek[bejelentkezve_index])
+            case '2':
+                szures(bejelentkezve_index)
+            case '0':
+                pass
             
         
             
@@ -60,43 +61,71 @@ def lehet(szemely: Tarskereso, jelenlegi: Tarskereso):
 def emberek_ajanlasa(bejelentkezve: Tarskereso):
     kilep = False
     index = 0
+    van_ember = False
     while not kilep:
         kovetkezo = emberek[index]
         while not lehet(kovetkezo, bejelentkezve) and index < len(emberek)-1:
             index += 1
             kovetkezo = emberek[index]
-        if index == len(emberek)-1 and valasztas == '':
-            print('Sajnos nincs számodra megfelelő felhasználó :(')
+        if index == len(emberek)-1 and not van_ember:
+            print('Sajnos nincs számodra megfelelő felhasználó :( (próbáld meg változtatni a szűrőket)')
+            input('<ENTER>')
             kilep = True
         elif index == len(emberek)-1 and valasztas != '':
             index = 0
         else:
+            van_ember = True
             index += 1
             os.system('cls')
             print(f'{kovetkezo.vezeteknev} {kovetkezo.keresztnev}')
             print(f'Kor: {kovetkezo.kor}')
             print(f'Távolság tőled: {kovetkezo.tavolsag_toled} km')
+            print(f'Gyerekek száma: {kovetkezo.gyerekek}')
             valasztas = input('\nBalra húz(B) / Jobbra húz(J) / Kilép(K): ')
             while valasztas != 'B' and valasztas != 'J' and valasztas != 'K':
                 valasztas = input('\nBalra húz(B) / Jobbra húz(J) / Kilép(K): ')
             if valasztas == 'K':
                 kilep = True
-            elif valasztas == 'B':
-                pass
-            else:
+            elif valasztas == 'J':
                 pass
 
-
-def szures(ember: Tarskereso):
-    os.system('cls')
-    print('Válassza ki, mit szeretne MÓDOSítani:')
-    print(f'\t1.: Keresett kor alsó határ (jelenlegi: {ember.keresett_kor_also_hatar} év)')
-    print(f'\t2.: Keresett kor felső határ (jelenlegi: {ember.keresett_kor_felso_hatar})')
-    print(f'\t3.: Minimális gyerekszám (jelenlegi: {ember.keresett_gyerek_also_hatar})')
-    print(f'\t4.: Maximális gyerekszám (jelenlegi: {ember.keresett_gyerek_felso_hatar})')
-    valasztas = input('Választásom: ')
-    while valasztas != '1' and valasztas != '2' and valasztas != '3' and valasztas != '4':
+def szures(index: int):
+    valasztas = ''
+    while valasztas != '0':
+        os.system('cls')
+        print('Válassza ki, mit szeretne MÓDOSítani:')
+        print(f'\t1.: Keresett kor alsó határ (jelenlegi: {emberek[index].keresett_kor_also_hatar} év)')
+        print(f'\t2.: Keresett kor felső határ (jelenlegi: {emberek[index].keresett_kor_felso_hatar})')
+        print(f'\t3.: Minimális gyerekszám (jelenlegi: {emberek[index].keresett_gyerek_also_hatar})')
+        print(f'\t4.: Maximális gyerekszám (jelenlegi: {emberek[index].keresett_gyerek_felso_hatar})')
+        print('\t0.: Kilépés')
         valasztas = input('Választásom: ')
+        while valasztas != '1' and valasztas != '2' and valasztas != '3' and valasztas != '4' and valasztas != '0':
+            valasztas = input('Választásom: ')
+        match valasztas:
+            case '1':
+                uj = input('A keresett kor új alsó határa: ')
+                while not uj.isdigit():
+                    uj = input('A keresett kor új alsó határa: ')
+                emberek[index].keresett_kor_also_hatar = int(uj)
+
+            case '2':
+                uj = input('A keresett kor új felső határa: ')
+                while not uj.isdigit():
+                    uj = input('A keresett kor új felső határa: ')
+                emberek[index].keresett_kor_felso_hatar = int(uj)
+            case '3':
+                uj = input('Új minimális gyerekszám: ')
+                while not uj.isdigit():
+                    uj = input('Új minimális gyerekszám: ')
+                emberek[index].keresett_gyerek_also_hatar = int(uj)
+            case '4':
+                uj = input('Új maximális gyerekszám: ')
+                while not uj.isdigit():
+                    uj = input('Új maximális gyerekszám: ')
+                emberek[index].keresett_gyerek_felso_hatar = int(uj)
+    kiiras()
+
     
         
             
@@ -128,7 +157,15 @@ def randomolas():
         keresett_also = randint(0, 4)
         keresett_felso = randint(keresett_also, 4)
 
-        f.write(f'{vezeteknevek[i]};{keresztnevek[i]};{korok[i]};{tavolsagok[i]};{nemek[i]};{keresett_nemek[i]};{max(18, int(korok[i])-7)};{min(70, int(korok[i])+7)};{gyerekek};{randint(1, 4)};\n')
+        f.write(f'{vezeteknevek[i]};{keresztnevek[i]};{korok[i]};{tavolsagok[i]};{nemek[i]};{keresett_nemek[i]};{max(18, int(korok[i])-7)};{min(70, int(korok[i])+7)};{gyerekek};{keresett_felso};{keresett_also}\n')
+
+def kiiras():
+    f = open('python/szingli_anyukak_es_apukak_es_gyerekek.csv', 'w', encoding='utf-8')
+
+    for e in emberek:
+        f.write(f'{e.vezeteknev};{e.keresztnev};{e.kor};{e.tavolsag_toled};{int(e.nem)};{int(e.keresett_nem)};{e.keresett_kor_also_hatar};{e.keresett_kor_felso_hatar};{e.gyerekek};{e.keresett_gyerek_felso_hatar};{e.keresett_gyerek_also_hatar}\n')
+
+    f.close()
     
 def login_feltoltes():
     f = open('python/szingli_anyukak_es_apukak_es_gyerekek.csv', 'r', encoding='utf-8')
@@ -136,4 +173,4 @@ def login_feltoltes():
     for sor in f:
         f2.write(f'{Tarskereso(sor).keresztnev};{Tarskereso(sor).keresztnev.lower()}123\n')
 
-main()      
+main()
--- python/osztaly.py
+++ python/osztaly.py
@@ -6,7 +6,7 @@ class Tarskereso:
         self.kor = int(adatok[2])
         self.tavolsag_toled = int(adatok[3])
         self.nem = bool(adatok[4]=='1')      #0: nő   1: férfi
-        self.keresett_nem = bool(adatok[5])
+        self.keresett_nem = bool(adatok[5]=='1')
         self.keresett_kor_also_hatar = int(adatok[6])
         self.keresett_kor_felso_hatar = int(adatok[7])
         self.gyerekek = int(adatok[8])
